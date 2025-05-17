31c91c64875c46faeea4f67afa6d2dbe9e1038b3
Luzatti-alt <lucasguzatti4@gmail.com>
2025-05-17 17:20:23 -0300

    Update App_mundo_senai.java
    adicionei algumas coisas que sei que vão precisar mas nada realmente funcional, somente as ideias que lembrei que vão estar no aplicativo para poder adicionar a tela, mas nenhum funcionamento

--- aplicativo/App_mundo_senai.java
+++ aplicativo/App_mundo_senai.java
@@ -4,8 +4,7 @@ import java.awt.event.*;
 import java.io.*;
 
 public class App_mundo_senai extends JFrame implements ActionListener{
-	//salvo_sim
-	//skip de login acima(tem que estar em info.txt)
+	//salvo_sim //skip de login acima(tem que estar em info.txt)
 	//componentes globais
 	JFrame janela = new JFrame();
 	JButton Logar = new JButton();
@@ -15,52 +14,6 @@ public class App_mundo_senai extends JFrame implements ActionListener{
 	JPanel Deco = new JPanel();//painel onde ficara a parte visial do Login
 	JPanel dados = new JPanel();//painel para por infos para Logar
 	//código do aplicativo
-	public void login(){
-		//metodo ⬆️ para o sistema de login 
-		//aspectos da janela 
-		janela.add(Deco);
-		janela.setTitle("Projeto Mundo Senai: AAAAAAA");
-		janela.setMinimumSize(new Dimension(100,100));
-		janela.setSize(new Dimension(1000, 500));
-		janela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		janela.setLayout(new GridLayout(1,3));
-		//onde fica as informações p/Login
-		JPanel inv1 = new JPanel();
-		JPanel inv2 = new JPanel();
-		dados.setLayout(new GridLayout(5,1));
-		dados.add(inv1);
-		dados.add(usuario_login);
-		dados.add(senha_login);
-		dados.add(Logar);
-		dados.add(inv2);
-		//configurando o tamanho
-		usuario_login.setPreferredSize(new Dimension(100,100));
-		senha_login.setPreferredSize(new Dimension(100,100));
-		janela.add(dados);
-		Logar.setFocusable(false);
-		Logar.setText("Logar");
-		Logar.setBounds(100,100,100,100);
-		Logar.addActionListener(this);
-		//visibilidade de tudo
-		janela.setVisible(true);
-		JPanel inv3 = new JPanel();
-		janela.add(inv3);
-	}
-	public void menu(){
-		//metodo ⬆️ para o menu
-		//aspectos da janela 
-		janela.setTitle("Projeto Mundo Senai: AAAAAAA");
-		janela.setMinimumSize(new Dimension(100,100));
-		janela.setSize(new Dimension(1000, 500));
-		janela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		 janela.getContentPane().removeAll();
-		 // reset layout e background
-		 janela.getContentPane().setBackground(Color.BLACK);
-		 janela.setLayout(null);//trocar qnd desing for enviado
-		 // reseta a janela
-		 janela.revalidate();
-		 janela.repaint();
-	}
 	public App_mundo_senai() {
 		//obj ⬆️ para inicializar
 		//add save de info de login em info.txt e assim na inicialização
@@ -99,11 +52,98 @@ public class App_mundo_senai extends JFrame implements ActionListener{
             e.printStackTrace();
         }
 	}
-	//para o botão funcionar tem que estar no mesmo arquivo e ser em java
-	//sumida esta com a parte do backend mas está parte é do frontend 
-	//pois tenho que adicionar a funcionalidade pelo frontend
-	//o mesmo pro skip da parte de login se estiver salvo na máquina
-	//funcionamento do botão
+
+	public void login(){
+		//metodo ⬆️ para o sistema de login 
+		//aspectos da janela 
+		janela.add(Deco);
+		janela.setTitle("Projeto Mundo Senai: AAAAAAA");
+		janela.setMinimumSize(new Dimension(100,100));
+		janela.setSize(new Dimension(1000, 500));
+		janela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		janela.setLayout(new GridLayout(1,3));
+		//onde fica as informações p/Login
+		JPanel inv1 = new JPanel();
+		JPanel inv2 = new JPanel();
+		dados.setLayout(new GridLayout(5,1));
+		dados.add(inv1);
+		dados.add(usuario_login);
+		dados.add(senha_login);
+		dados.add(Logar);
+		dados.add(inv2);
+		//configurando o tamanho
+		usuario_login.setPreferredSize(new Dimension(100,100));
+		senha_login.setPreferredSize(new Dimension(100,100));
+		janela.add(dados);
+		Logar.setFocusable(false);
+		Logar.setText("Logar");
+		Logar.setBounds(100,100,100,100);
+		Logar.addActionListener(this);
+		//visibilidade de tudo
+		janela.setVisible(true);
+		JPanel inv3 = new JPanel();
+		janela.add(inv3);
+	}
+
+	public void menu(){
+		//metodo ⬆️ para o menu
+		//aspectos da janela 
+		janela.setTitle("Projeto Mundo Senai: AAAAAAA");
+		janela.setMinimumSize(new Dimension(100,100));
+		janela.setSize(new Dimension(1000, 500));
+		janela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		 janela.getContentPane().removeAll();
+		 // reset layout e background
+		 janela.getContentPane().setBackground(Color.BLACK);
+		 janela.setLayout(null);//trocar qnd desing for enviado
+		 // reseta a janela
+		 janela.revalidate();
+		 janela.repaint();
+	}
+
+	public void Loja_pnts(){
+		janela.setTitle("Projeto Mundo Senai: AAAAAAA");
+		janela.setMinimumSize(new Dimension(100,100));
+		janela.setSize(new Dimension(1000, 500));
+		janela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		 janela.getContentPane().removeAll();
+		 // reset layout e background
+		 janela.getContentPane().setBackground(Color.BLACK);
+		 janela.setLayout(null);//trocar qnd desing for enviado
+		 // reseta a janela
+		 janela.revalidate();
+		 janela.repaint();
+	}
+
+	public void dados_pessoais(){
+		janela.setTitle("Projeto Mundo Senai: AAAAAAA");
+		janela.setMinimumSize(new Dimension(100,100));
+		janela.setSize(new Dimension(1000, 500));
+		janela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		 janela.getContentPane().removeAll();
+		 // reset layout e background
+		 janela.getContentPane().setBackground(Color.BLACK);
+		 janela.setLayout(null);//trocar qnd desing for enviado
+		 // reseta a janela
+		 janela.revalidate();
+		 janela.repaint();
+	}
+
+	public void quests(){
+		janela.setTitle("Projeto Mundo Senai: AAAAAAA");
+		janela.setMinimumSize(new Dimension(100,100));
+		janela.setSize(new Dimension(1000, 500));
+		janela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		 janela.getContentPane().removeAll();
+		 // reset layout e background
+		 janela.getContentPane().setBackground(Color.BLACK);
+		 janela.setLayout(null);//trocar qnd desing for enviado
+		 // reseta a janela
+		 janela.revalidate();
+		 janela.repaint();
+	}
+	//para o botão funcionar tem que estar no mesmo arquivo e ser em java sumida esta com a parte do backend mas está parte é do frontend 
+	//pois tenho que adicionar a funcionalidade pelo frontend o mesmo pro skip da parte de login se estiver salvo na máquina funcionamento do botão
 	@Override
 	public void actionPerformed(ActionEvent e){
         if(e.getSource() == Logar){
@@ -118,6 +158,7 @@ public class App_mundo_senai extends JFrame implements ActionListener{
 			menu();
 		}
 	}
+
 	//inicializa o aplicativo
 	public static void main(String[]args) {
 		new App_mundo_senai();
