0d7e84e1038eb663e870b776d321e312ab23c73f
VenueBruv <venue.bruv@gmail.com>
2025-05-19 10:42:22 -0300

    login e registro funcionando

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 # cria o app
 app = Flask(__name__)
+usuarios = {} # banco de dados simulado
 
 # rota basica
+# @app.route('/')
+# def home():
+#     return render_template("template teste.html", nome="Fellipe", teste="seilabro")
+
 @app.route('/')
 def home():
-    return render_template("template teste.html", nome="Fellipe", teste="seilabro")
+    return redirect(url_for('login'))
 
 # cria uma rota dinâmica
 @app.route('/usuario/<nome>')
@@ -19,10 +24,22 @@ def login():
     if request.method == 'POST':
         usuario = request.form['usuario']
         senha = request.form['senha']
-        return f'Usuário: {usuario}, Senha: {senha}'
-    return  '''
-            
-            '''
+        if usuario in usuarios and usuarios[usuario] == senha:
+            return render_template('dashboard.html', nome=usuario)
+        else:
+            return 'Usuário ou senha incorretos'
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        usuario = request.form['usuario']
+        senha = request.form['senha']
+        if usuario in usuarios:
+            return 'Usuário já existe'
+        usuarios[usuario] = senha
+        return redirect(url_for('login'))
+    return render_template('sign_up.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
