ab6b10f1dd63fa42f6f71b6983c93891a89ee491
Luzatti-alt <lucasguzatti4@gmail.com>
2025-05-19 17:52:34 -0300

    Update App_mundo_senai.java

--- aplicativo/App_mundo_senai.java
+++ aplicativo/App_mundo_senai.java
@@ -35,8 +35,28 @@ public class App_mundo_senai extends JFrame implements ActionListener {
 	JButton Pix = new JButton("Pix");
 	JButton Credito = new JButton("Crédito");
 	JButton Boleto = new JButton("Boleto");
-	JPanel dados = new JPanel();
 	JPanel Info_conta = new JPanel();
+    //Metas
+    JTextArea metas_titulo = new JTextArea("Metas");
+    JPanel Sistema_metas = new JPanel();
+    JPanel Meta_diario = new JPanel();
+    JPanel Meta_semanal = new JPanel();
+    JPanel Meta_mensal = new JPanel();
+    JTextArea Metas_diarias_txt = new JTextArea("Diarias");
+    JTextArea Qual_diaria = new JTextArea("adicionar a metas diaria");
+    JTextArea Metas_semanais_txt = new JTextArea("Semanais");
+    JTextArea Qual_semanal = new JTextArea("adicionar a semanal");
+    JTextArea Metas_mensais_txt = new JTextArea("Mensais");
+    JTextArea Qual_mensal = new JTextArea("adicionar a mensal");
+    JTextArea pnts_e_comprovacao_diaria = new JTextArea("pontos");
+    JTextArea pnts_e_comprovacao_semanal= new JTextArea("pontos");
+    JTextArea pnts_e_comprovacao_mensal= new JTextArea("pontos");
+    JPanel dividir_dia = new JPanel();
+    JPanel dividir_semana = new JPanel();
+    JPanel dividir_mes = new JPanel();
+    JButton comprovar_meta_diaria = new JButton();
+    JButton comprovar_meta_semanal = new JButton();
+    JButton comprovar_meta_mensal = new JButton();
 	//skip se ja tiver uma conta logada do parte do  login
     public App_mundo_senai(){
         if (info.exists()) {
@@ -108,8 +128,9 @@ public class App_mundo_senai extends JFrame implements ActionListener {
         janela.add(Logar);
         janela.setVisible(true);
     }
-	public void Menus(){
-		 Menu = new JPanel(); // Recria o painel de menu
+	//menu em cima
+    public void Menus(){
+	Menu = new JPanel(); // Recria o painel de menu
     Menu.setBackground(Color.white);
     Menu.setBounds(0, 0, 1550, 28);
     Menu.setLayout(null); // Permite posicionamento absoluto
@@ -145,7 +166,8 @@ public class App_mundo_senai extends JFrame implements ActionListener {
     configs.addActionListener(this);
     Menu.add(configs);
 }
-	public void limparJanela(){
+	//resetar elementos
+    public void limparJanela(){
     janela.getContentPane().removeAll();
     janela.repaint();
     janela.revalidate();
@@ -156,80 +178,125 @@ public class App_mundo_senai extends JFrame implements ActionListener {
     janela.setMinimumSize(new Dimension(100, 100));
     janela.setSize(new Dimension(1000, 500));
     janela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-    janela.setLayout(new BorderLayout());
-	Menu.setBackground(Color.white);
+    janela.setLayout(null);
+    Info_conta.setLayout(null);
 	Info_conta.setBackground(Color.lightGray);
-	Info_conta.setLayout(null);
-	dados.setBackground(Color.gray);
-	dados.setLayout(null);
 	//titulo acima
+    janela.add(ops_assinatura);
 	ops_assinatura.setEditable(false);
 	ops_assinatura.setFont(new Font("Arial", Font.PLAIN, 30));
 	ops_assinatura.setBackground(Color.BLUE);
-	ops_assinatura.setBounds(0,28,1550,30);
-	//infos da conta
-	janela.add(ops_assinatura);
-	Info_conta.setLayout(new BorderLayout());
-	janela.add(Info_conta);
-	janela.add(Info_conta,BorderLayout.EAST);
-	janela.add(dados,BorderLayout.WEST);
+	ops_assinatura.setBounds(0,26,1550,35);
+    //infos da conta
+    janela.add(Info_conta);
 	Info_conta.setBackground(Color.gray);
-	Info_conta.setBounds(0,58,1550,900);
-	//mes e valor
-	Info_conta.add(mes);
+	Info_conta.setBounds(550,58,1000,1000);
+    //mes e valor
+	janela.add(mes);
 	mes.setEditable(false);
 	mes.setFont(new Font("Arial", Font.PLAIN, 26));
 	mes.setBounds(100,100,120,50);
-	Info_conta.add(tres_meses);
+	janela.add(tres_meses);
 	tres_meses.setEditable(false);
 	tres_meses.setFont(new Font("Arial", Font.PLAIN, 26));
 	tres_meses.setBounds(100,160,120,50);
-	Info_conta.add(seis_meses);
+	janela.add(seis_meses);
 	seis_meses.setEditable(false);
 	seis_meses.setFont(new Font("Arial", Font.PLAIN, 26));
 	seis_meses.setBounds(100,220,120,50);
-	Info_conta.add(doze_meses);
+	janela.add(doze_meses);
 	doze_meses.setEditable(false);
 	doze_meses.setFont(new Font("Arial", Font.PLAIN, 26));
 	doze_meses.setBounds(100,280,120,50);
-	Info_conta.add(cinquentao);
+	janela.add(cinquentao);
 	cinquentao.setEditable(false);
 	cinquentao.setFont(new Font("Arial", Font.PLAIN, 26));
 	cinquentao.setBounds(250,100,120,50);
-	Info_conta.add(cento_cinquente);
+	janela.add(cento_cinquente);
 	cento_cinquente.setEditable(false);
 	cento_cinquente.setFont(new Font("Arial", Font.PLAIN, 26));
 	cento_cinquente.setBounds(250,160,120,50);
-	Info_conta.add(trezentos);
+	janela.add(trezentos);
 	trezentos.setEditable(false);
 	trezentos.setFont(new Font("Arial", Font.PLAIN, 26));
 	trezentos.setBounds(250,220,120,50);
-	Info_conta.add(quinhentos_quarenta);
+	janela.add(quinhentos_quarenta);
 	quinhentos_quarenta.setEditable(false);
 	quinhentos_quarenta.setFont(new Font("Arial", Font.PLAIN, 26));
 	quinhentos_quarenta.setBounds(250,280,120,50);
 	//pagamento
-	Info_conta.add(Debito);
+	janela.add(Debito);
 	Debito.setFocusable(false);
 	Debito.setBounds(100,360,120,50);
-	Info_conta.add(Pix);
+	janela.add(Pix);
 	Pix.setFocusable(false);
 	Pix.setBounds(100,420,120,50);
-	Info_conta.add(Credito);
+	janela.add(Credito);
 	Credito.setFocusable(false);
 	Credito.setBounds(250,360,120,50);
-	Info_conta.add(Boleto);
+	janela.add(Boleto);
 	Boleto.setFocusable(false);
 	Boleto.setBounds(250,420,120,50);
 	//outros
     janela.getContentPane().setBackground(Color.BLACK);
-    janela.setLayout(null); 
     janela.revalidate();
     janela.repaint();
     }
-	//outra pagina
+	//pagina de metas
 	public void Metas(){
-		//
+        janela.add(metas_titulo);
+        metas_titulo.setBackground(Color.blue);
+        metas_titulo.setBounds(0, 26, 1550, 35);
+        metas_titulo.setFont(new Font("Arial", Font.PLAIN, 30));
+        janela.add(Sistema_metas);
+        Sistema_metas.setBackground(Color.white);
+        Sistema_metas.setLayout(new GridLayout(3,1));
+        Sistema_metas.setBounds(0,61,1550,400);
+        //diarias
+        Sistema_metas.add(Meta_diario);
+        Meta_diario.add(Metas_diarias_txt);
+        Metas_diarias_txt.setEditable(false);
+        Metas_diarias_txt.setFont(new Font("Arial", Font.PLAIN, 30));
+        Meta_diario.setLayout(new GridLayout(3,1));
+        Meta_diario.add(Qual_diaria);
+        Qual_diaria.setEditable(false);
+        pnts_e_comprovacao_diaria.setEditable(false);
+        Meta_diario.add(dividir_dia);
+        dividir_dia.setLayout(new GridLayout(1,2));
+        dividir_dia.add(pnts_e_comprovacao_diaria);
+        dividir_dia.add(comprovar_meta_diaria);
+        comprovar_meta_diaria.setFocusable(false);
+        comprovar_meta_diaria.addActionListener(this);
+        //semanal
+        Sistema_metas.add(Meta_semanal);
+        Meta_semanal.add(Metas_semanais_txt);
+        Metas_semanais_txt.setEditable(false);
+        Metas_semanais_txt.setFont(new Font("Arial", Font.PLAIN, 30));
+        Meta_semanal.setLayout(new GridLayout(3,1));
+        Meta_semanal.add(Qual_semanal);
+        Qual_semanal.setEditable(false);
+        pnts_e_comprovacao_semanal.setEditable(false);
+        Meta_semanal.add(dividir_semana);
+        dividir_semana.setLayout(new GridLayout(1,2));
+        dividir_semana.add(pnts_e_comprovacao_semanal);
+        dividir_semana.add(comprovar_meta_semanal);
+        comprovar_meta_semanal.setFocusable(false);
+        comprovar_meta_semanal.addActionListener(this);
+        //mensal
+        Sistema_metas.add(Meta_mensal);
+        Meta_mensal.add(Metas_mensais_txt);
+        Metas_mensais_txt.setEditable(false);
+        Metas_mensais_txt.setFont(new Font("Arial", Font.PLAIN, 30));
+        Meta_mensal.setLayout(new GridLayout(3,1));
+        Meta_mensal.add(Qual_mensal);
+        Qual_mensal.setEditable(false);
+        pnts_e_comprovacao_mensal.setEditable(false);
+        Meta_mensal.add(dividir_mes);
+        dividir_mes.setLayout(new GridLayout(1,2));
+        dividir_mes.add(pnts_e_comprovacao_mensal);
+        dividir_mes.add(comprovar_meta_mensal);
+        comprovar_meta_mensal.setFocusable(false);
+        comprovar_meta_mensal.addActionListener(this);
 	}
     // Para o botão funcionar
     @Override
@@ -271,7 +338,13 @@ public class App_mundo_senai extends JFrame implements ActionListener {
 			//
 		}if (e.getSource() == Boleto) {
 			//
-		}
+		}if(e.getSource()==comprovar_meta_diaria){
+            //
+        }if(e.getSource()==comprovar_meta_semanal){
+            //
+        }if(e.getSource()==comprovar_meta_mensal){
+            //
+        }
     }
     // Inicializa o aplicativo
     public static void main(String[] args) {
