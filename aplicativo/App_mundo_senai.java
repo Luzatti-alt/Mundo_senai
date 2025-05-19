7834630735503501af5c98d397a2701f67ec204a
Luzatti-alt <lucasguzatti4@gmail.com>
2025-05-19 09:57:49 -0300

    Update App_mundo_senai.java

--- aplicativo/App_mundo_senai.java
+++ aplicativo/App_mundo_senai.java
@@ -1,137 +1,114 @@
-package aa;
-
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
+import javax.swing.*;
 
-public class App_mundo_senai extends JFrame implements ActionListener{
-	//salvo_sim //skip de login acima(tem que estar em info.txt)
-	//componentes globais
-	JFrame janela = new JFrame();
-	JButton Logar = new JButton();
-	File info = new File("info.txt");
-	JPanel inv = new JPanel();
-	JTextField usuario_login = new JTextField();
-	JTextField senha_login = new JTextField();
-	JTextArea usuario_text = new JTextArea("Usuario");
-	JTextArea senha_text = new JTextArea("Senha");
-	JPanel box_login = new JPanel();
-	JPanel Deco = new JPanel();//painel onde ficara a parte visial do Login
-	JPanel dados = new JPanel();//painel para por infos para Logar
+public class App_mundo_senai extends JFrame implements ActionListener {
+    // Componentes globais
+    JFrame janela = new JFrame();
+    JButton Logar = new JButton();
+    File info = new File("info.txt");
+    JPanel inv = new JPanel();
+    JTextField usuario_login = new JTextField();
+    JTextField senha_login = new JTextField();
+    JTextArea usuario_text = new JTextArea("Usuario");
+    JTextArea senha_text = new JTextArea("Senha");
+    JPanel box_login = new JPanel();
+    JPanel Deco = new JPanel(); // Painel onde ficará a parte visual do Login
 
-	//código do aplicativo
-	public App_mundo_senai() {
-		//obj ⬆️ para inicializar
-		//add save de info de login em info.txt e assim na inicialização
-		//verificar se o arquivo de informações existe cria se não tiver
-		if(info.exists()){
-            System.out.println("arquivo existente");
-        }else{
-			System.out.println("arquivo inexistente");
-            try{
+    // Código do aplicativo
+    public App_mundo_senai() {
+        if (info.exists()) {
+            System.out.println("Arquivo existente");
+        } else {
+            System.out.println("Arquivo inexistente");
+            try {
                 FileWriter info = new FileWriter("info.txt");
-                //cria o arquuvo
-                info.write("add info de login talvez via pipe");//na parte com o server
-                info.close();//metodo de fechar o arquivo e salvar 
-                //é necessario sair do arquivo para q o conteudo esteja nele
-            }catch (IOException e) {
-            e.printStackTrace();//sem o catch nn vai funcionar a craição do arquivo
-        }}
-		//se ja tiver informação de login salva pular metodo de login
-			String validador_login = "salvo_sim";
-			//palavra no arquivo que se está lá ja pula pra página principal
-			try {
+                info.write("add info de login talvez via pipe");
+                info.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        String validador_login = "salvo_sim";
+        try {
             BufferedReader leitor = new BufferedReader(new FileReader(info));
             String linha;
-			//o login_state tem que estar na primeira linha
             while ((linha = leitor.readLine()) != null) {
                 if (linha.equals(validador_login)) {
-					menu();
-                    break; 
-                }else{
-					login();
-					break;
-				}
+                    menu();
+                    break;
+                } else {
+                    login();
+                    break;
+                }
             }
             leitor.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-	}
+    }
 
-	public void login(){
-		janela.setTitle("Projeto Mundo Senai: AAAAAAA");
-		janela.setMinimumSize(new Dimension(100,100));
-		janela.setSize(new Dimension(1000, 500));
-		janela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		janela.setLayout(null);
-		//aspectos da janela 
-		//criar metodo que centralize em x conforme tamanho horizontal da tela
-		box_login.setLayout(new GridLayout(4,1));
-		janela.add(box_login);
-		janela.add(Logar);
-		Logar.setBounds(100,100,100,100);
-		janela.add(inv);
-		inv.setSize(new Dimension(2,1));
-		box_login.setBackground(Color.gray);
-		box_login.setBounds(100,100,500,500);
-		box_login.add(usuario_text);
-		usuario_text.setBackground(null);
-		usuario_text.setFont(getFont());
-		box_login.add(usuario_login);
-		usuario_text.setFont(new Font("arial",Font.PLAIN,80));
-		box_login.add(senha_text);
-		senha_text.setFont(new Font("arial",Font.PLAIN,80));
-		senha_text.setBackground(null);
-		box_login.add(senha_login);
-		janela.add(Deco);
-		//onde fica as informações p/Login
-		Logar.setFocusable(false);
-		Logar.setSize(new Dimension(100,100));
-		Logar.setLocation(500, 700);
-		Logar.setText("Logar");
-		Logar.addActionListener(this);
-		//visibilidade de tudo
-		janela.setVisible(true);
-	}
-	public void menu(){
-		//metodo ⬆️ para o menu
-		//aspectos da janela 
-		janela.setTitle("Projeto Mundo Senai: AAAAAAA");
-		janela.setMinimumSize(new Dimension(100,100));
-		janela.setSize(new Dimension(1000, 500));
-		janela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		janela.getContentPane().removeAll();
-		// reset layout e background
-		janela.getContentPane().setBackground(Color.BLACK);
-		janela.setLayout(null);//trocar qnd desing for enviado
-		// reseta a janela
-		janela.revalidate();
-		janela.repaint();
-	}
+    public void login() {
+        janela.setTitle("Projeto Mundo Senai: AAAAAAA");
+        janela.setMinimumSize(new Dimension(100, 100));
+        janela.setSize(new Dimension(1000, 500));
+        janela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        janela.setLayout(null); 
+        box_login.setLayout(null);
+        box_login.setBounds(100, 100, 500, 500);
+        box_login.setBackground(Color.GRAY);
+        usuario_text.setBounds(10, 10, 480, 60);
+        usuario_text.setBackground(null);
+        usuario_text.setEditable(false);
+        usuario_text.setFont(new Font("Arial", Font.PLAIN, 30));
+        box_login.add(usuario_text);
 
-	//para o botão funcionar tem que estar no mesmo arquivo e ser em java sumida esta com a parte do backend mas está parte é do frontend 
-	//pois tenho que adicionar a funcionalidade pelo frontend o mesmo pro skip da parte de login se estiver salvo na máquina funcionamento do botão
-	//funcionamento de botões
-	@Override
-	public void actionPerformed(ActionEvent e){
-        if(e.getSource() == Logar){
-			//apertar botão logar e adicionat condição valido pra fazer isto
-			System.out.println("logando");
-			janela.remove(box_login);
-			janela.remove(Logar);
-			janela.getContentPane().setBackground(Color.BLACK);
-			//atualiza/refresh na janela sem isso congela(visualmente) o botão
-			janela.revalidate();
-			janela.repaint();
-			//dos ir pro menu
-			menu();
-		}
-	}
+        usuario_login.setBounds(10, 80, 480, 40);
+        box_login.add(usuario_login);
 
-	//inicializa o aplicativo
-	public static void main(String[]args) {
-		new App_mundo_senai();
-	}
+        senha_text.setBounds(10, 140, 480, 60);
+        senha_text.setBackground(null);
+        senha_text.setEditable(false);
+        senha_text.setFont(new Font("Arial", Font.PLAIN, 30)); 
+        box_login.add(senha_text);
+        senha_login.setBounds(10, 210, 480, 40); 
+        box_login.add(senha_login);
+        janela.add(box_login);
+        Logar.setText("Logar");
+        Logar.setBounds(200, 400, 100, 50);  
+        Logar.addActionListener(this);
+        janela.add(Logar);
+        janela.setVisible(true);
+    }
+
+    public void menu() {
+        janela.setTitle("Projeto Mundo Senai: AAAAAAA");
+        janela.setMinimumSize(new Dimension(100, 100));
+        janela.setSize(new Dimension(1000, 500));
+        janela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        janela.getContentPane().removeAll();
+        janela.getContentPane().setBackground(Color.BLACK);
+        janela.setLayout(null); 
+        janela.revalidate();
+        janela.repaint();
+    }
+    // Para o botão funcionar
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == Logar) {
+            System.out.println("Logando...");
+            janela.remove(box_login);
+            janela.remove(Logar);
+            janela.getContentPane().setBackground(Color.BLACK);
+            janela.revalidate();
+            janela.repaint();
+            menu();
+        }
+    }
+    // Inicializa o aplicativo
+    public static void main(String[] args) {
+        new App_mundo_senai();
+    }
 }
