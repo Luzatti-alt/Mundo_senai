e9d533257a37b1992aa260f7523f2eb26714935e
VenueBruv <venue.bruv@gmail.com>
2025-05-28 09:22:08 -0300

    adição da integração de api de notícias

--- site/website/views.py
+++ site/website/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
+from .news import fetch_news
 
 views = Blueprint('views', __name__)
 
@@ -7,4 +8,11 @@ views = Blueprint('views', __name__)
 @views.route('/')
 @login_required
 def home():
-    return render_template('home.html', nome=current_user.nome, user=current_user)
+    noticias = fetch_news()
+    
+    return render_template(
+        'home.html',
+        nome=current_user.nome,
+        noticias=noticias,
+        user=current_user
+        )
