da06ae457416d67ffa57fecdf6df1a0dde7c2efd
Luzatti-alt <lucasguzatti4@gmail.com>
2025-06-02 18:39:09 -0300

    Update request.java
    esta versão pega todos os dados

--- aplicativo/request.java
+++ aplicativo/request.java
@@ -4,17 +4,20 @@ public class request {
     public static void main(String[] args) throws Exception {
         //txt com as infos p/ver se é compativel
         File client = new File("teste.txt");
+        String linhas;
+        if (client.exists()) {
+            System.out.println("teste");
+        }
         //tenta se conectar com a api 
-        URL url_login = new URL("http://10.158.152.21:5000/api/usuarios");
+        URL url_login = new URL("http://192.168.100.34:5000/api/usuarios");
         HttpURLConnection conectar = (HttpURLConnection) url_login.openConnection();
         //ver se está conectado
+        conectar.setRequestMethod("GET");
         int resposta = conectar.getResponseCode();
         System.out.println("resp: "+resposta);
         //metodo de request
-        conectar.setRequestMethod("GET");
-        //URL url_modelos = new URL("http://10.158.152.21:5000/api/modelos");
-        //URL url_tarefas = new URL("http://10.158.152.21:5000/api/tarefas");
-
+        URL url_modelos = new URL("http://10.158.152.21:5000/api/modelos");
+        URL url_tarefas = new URL("http://10.158.152.21:5000/api/tarefas");
         //configurações da conecção
         int tamanho = conectar.getContentLength();
         System.out.println("lenght: "+tamanho);
@@ -23,22 +26,20 @@ public class request {
         //401 nn foi autorizado deu 
         //agr foi 200 agr deu
         //leitor de dados por enquanto preciso da  chave
-        BufferedReader ler = new BufferedReader(new InputStreamReader(conectar.getInputStream()));
-        System.out.println(ler);
-        if (client.exists()) {
-            System.out.println("teste");
-        } else {
+        if(client.exists()){
+            System.out.println("arquivo nn existe criand ele");
+            BufferedReader ler = new BufferedReader(new InputStreamReader(conectar.getInputStream()));
             try {
-                FileWriter client_txt = new FileWriter("teste.txt");
-                String info = ler.readLine();
-                String info2 = ler.readLine()+1;
-                client_txt.write(info);
-                client_txt.write(info2);
-                client_txt.close();
-            System.out.println(info);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+                FileWriter client_txt = new FileWriter("teste.txt", true);
+            while ((linhas = ler.readLine()) != null) {
+            client_txt.write(linhas + "\n");
+            System.out.println(linhas);
+        }
+        client_txt.close();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+    System.out.println(ler);
         }
     }
 }
