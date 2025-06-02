19d5df78aad9577df2e142a73553be4a8d729eca
Luzatti-alt <lucasguzatti4@gmail.com>
2025-06-02 11:42:33 -0300

    Update request.java

--- aplicativo/request.java
+++ aplicativo/request.java
@@ -7,23 +7,21 @@ public class request {
         //tenta se conectar com a api 
         URL url_login = new URL("http://10.158.152.21:5000/api/usuarios");
         HttpURLConnection conectar = (HttpURLConnection) url_login.openConnection();
+        //ver se está conectado
+        int resposta = conectar.getResponseCode();
+        System.out.println("resp: "+resposta);
+        //metodo de request
         conectar.setRequestMethod("GET");
-        URL url_modelos = new URL("http://10.158.152.21:5000/api/modelos");
-        URL url_tarefas = new URL("http://10.158.152.21:5000/api/tarefas");
+        //URL url_modelos = new URL("http://10.158.152.21:5000/api/modelos");
+        //URL url_tarefas = new URL("http://10.158.152.21:5000/api/tarefas");
+
         //configurações da conecção
         int tamanho = conectar.getContentLength();
         System.out.println("lenght: "+tamanho);
-        conectar.setConnectTimeout(1000); // 50 secs
+        conectar.setConnectTimeout(1000); // 1 secs
         conectar.setReadTimeout(1000); 
-        //ver se está conectado
-        int resposta = conectar.getResponseCode();
         //401 nn foi autorizado deu 
-
-        //conectar.setRequestMethod(null);
-        //key e value para pooder ter acesso a api
-
-        //conectar.setRequestProperty("chave", "valor");
-        System.out.println("resp: "+resposta);
+        //agr foi 200 agr deu
         //leitor de dados por enquanto preciso da  chave
         BufferedReader ler = new BufferedReader(new InputStreamReader(conectar.getInputStream()));
         System.out.println(ler);
@@ -33,13 +31,14 @@ public class request {
             try {
                 FileWriter client_txt = new FileWriter("teste.txt");
                 String info = ler.readLine();
+                String info2 = ler.readLine()+1;
                 client_txt.write(info);
+                client_txt.write(info2);
                 client_txt.close();
             System.out.println(info);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-        System.out.println(conectar.getContent());
     }
 }
