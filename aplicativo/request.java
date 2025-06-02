0c809fb3ca194560ebc70728b77e444d0f9ed44a
Luzatti-alt <lucasguzatti4@gmail.com>
2025-06-02 11:37:26 -0300

    Update request.java
    tentar pegar a api inteira

--- aplicativo/request.java
+++ aplicativo/request.java
@@ -11,25 +11,31 @@ public class request {
         URL url_modelos = new URL("http://10.158.152.21:5000/api/modelos");
         URL url_tarefas = new URL("http://10.158.152.21:5000/api/tarefas");
         //configurações da conecção
-        conectar.setConnectTimeout(50000); // 50 secs
-        conectar.setReadTimeout(50000); 
+        int tamanho = conectar.getContentLength();
+        System.out.println("lenght: "+tamanho);
+        conectar.setConnectTimeout(1000); // 50 secs
+        conectar.setReadTimeout(1000); 
         //ver se está conectado
         int resposta = conectar.getResponseCode();
         //401 nn foi autorizado deu 
+
+        //conectar.setRequestMethod(null);
+        //key e value para pooder ter acesso a api
+
+        //conectar.setRequestProperty("chave", "valor");
         System.out.println("resp: "+resposta);
+        //leitor de dados por enquanto preciso da  chave
         BufferedReader ler = new BufferedReader(new InputStreamReader(conectar.getInputStream()));
         System.out.println(ler);
         if (client.exists()) {
             System.out.println("teste");
-            FileWriter client_txt = new FileWriter("teste.txt");
-            String info = ler.readLine();
-            client_txt.write(info);
-            client_txt.close();
         } else {
             try {
                 FileWriter client_txt = new FileWriter("teste.txt");
-                client_txt.write("aaa" + ler);
+                String info = ler.readLine();
+                client_txt.write(info);
                 client_txt.close();
+            System.out.println(info);
             } catch (IOException e) {
                 e.printStackTrace();
             }
