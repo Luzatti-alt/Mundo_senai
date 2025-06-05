32974e164ed483fa63b37ec14dd2228fb6bfd393
Luzatti-alt <lucasguzatti4@gmail.com>
2025-06-05 19:18:57 -0300

    Update Descricao.java

--- aplicativo/Descricao.java
+++ aplicativo/Descricao.java
@@ -1,8 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 public class Descricao extends JPanel implements ActionListener, ComponentListener{
     JTextArea titulo = new JTextArea();
+    JButton prod = new JButton();
+    JLabel descricao = new JLabel();
     JButton produtos = new JButton("Produtos");
     JButton quests = new JButton("Metas");
     JButton parceiros = new JButton("Parceiros");
@@ -11,9 +16,45 @@ public class Descricao extends JPanel implements ActionListener, ComponentListen
     JPanel desc = new JPanel();
     JButton dinheiro = new JButton("Comprar");
     JButton volte = new JButton("voltar");
+    ImageIcon Halter_original = new ImageIcon(getClass().getResource("/imagens/halter.png"));
+    Image Halter_img = Halter_original.getImage().getScaledInstance(96, 96, Image.SCALE_SMOOTH);
+    ImageIcon Halter_icon = new ImageIcon(Halter_img);
+    ImageIcon Whey_original = new ImageIcon(getClass().getResource("/imagens/whey.jpg"));
+    Image Whey_img = Whey_original.getImage().getScaledInstance(96, 96, Image.SCALE_SMOOTH);
+    ImageIcon Whey_icon = new ImageIcon(Whey_img);
+    ImageIcon bar_prot_original = new ImageIcon(getClass().getResource("/imagens/barra_prot.png"));
+    Image bar_prot_img = bar_prot_original.getImage().getScaledInstance(96, 96, Image.SCALE_SMOOTH);
+    ImageIcon bar_prot_icon = new ImageIcon(bar_prot_img);
+    ImageIcon toalha_original = new ImageIcon(getClass().getResource("/imagens/toalha.jpg"));
+    Image toalha_img = toalha_original.getImage().getScaledInstance(96, 96, Image.SCALE_SMOOTH);
+    ImageIcon toalha_icon = new ImageIcon(toalha_img);
     public void descricao(){
+        try{
+    FileReader leitor = new FileReader("produto.txt");
+    BufferedReader br = new BufferedReader(leitor);
+    String qual_prod = br.readLine(); 
+            if (qual_prod == "halter") {
+                prod.setIcon(Halter_icon);
+                descricao.setText("");
+            }else if (qual_prod == "barra") {
+                prod.setIcon(bar_prot_icon);
+                descricao.setText("");
+            }else if (qual_prod == "whey") {
+                prod.setIcon(Whey_icon);
+                descricao.setText("");
+            }else if (qual_prod == "toalha") {
+                prod.setIcon(toalha_icon);
+                descricao.setText("");
+            }
+            br.close();
+            leitor.close();
+    }catch(IOException ex){
+        ex.printStackTrace();
+    }
         //add funcionalidade para produtos especificos
         this.add(produtos);
+        item.add(prod);
+        desc.add(prod);
         produtos.addActionListener(this);
         this.add(quests);
         quests.addActionListener(this);
@@ -39,10 +80,8 @@ public class Descricao extends JPanel implements ActionListener, ComponentListen
         this.add(item);
         volte.addActionListener(this);
         item.setBackground(Color.lightGray);
-        //item.add();//imagem do item
         this.add(desc);
         desc.setBackground(Color.gray);
-        //desc.add();//descrição do produto
         this.add(dinheiro);
         this.add(titulo);
         titulo.setBackground(Color.green);
